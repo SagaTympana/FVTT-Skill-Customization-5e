29111302d1aa78dfb4fe1cd16d99650b8d8870cb
SagaTympana <96776279+SagaTympana@users.noreply.github.com>
2021-12-28 21:32:34 -0500

    Update skl-customization.js
    - Change entityClass to documentClass for Foundry 0.8.6.
    - Remove passive score calculation, as the dnd5e system handles this natively now.

--- skl-customization.js
+++ skl-customization.js
@@ -12,7 +12,7 @@ Hooks.once("setup", () => {
 Hooks.on("renderActorSheet", injectActorSheet);
 
 function patchActor5ePrepareData() {
-    libWrapper.register(MODULE_NAME, "CONFIG.Actor.entityClass.prototype.prepareData", function patchedPrepareData(wrapped, ...args) {
+    libWrapper.register(MODULE_NAME, "CONFIG.Actor.documentClass.prototype.prepareData", function patchedPrepareData(wrapped, ...args) {
         wrapped(...args);
 
         const skills = this.data.data.skills;
@@ -22,19 +22,13 @@ function patchActor5ePrepareData() {
             let bonusAsInt = parseInt(Number(bonus));
             if (!isNaN(bonusAsInt)) {
                 skill.total += bonusAsInt;
-
-                // recalculate passive score, taking observant feat into account
-                const observant = this.data.flags.dnd5e?.observantFeat;
-                const passiveBonus =
-                    observant && CONFIG.DND5E.characterFlags.observantFeat.skills.includes(key) ? 5 : 0;
-                skill.passive = 10 + skill.total + passiveBonus;
             }
         }
     }, "WRAPPER");
 }
 
 function patchActor5eRollSkill() {
-    libWrapper.register(MODULE_NAME, "CONFIG.Actor.entityClass.prototype.rollSkill", function patchedRollSkill(wrapped, ...args) {
+    libWrapper.register(MODULE_NAME, "CONFIG.Actor.documentClass.prototype.rollSkill", function patchedRollSkill(wrapped, ...args) {
         const [ skillId, options ] = args;
         const skillBonus = this.getFlag(MODULE_NAME, `${skillId}.${SKILL_BONUS_KEY}`);
         if (skillBonus) {
